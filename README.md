0916bb5d2af25a2fb384bfdc04062eb477d40b98
Nicolas Ritouet <nicolas.ritouet@zanox.com>
2014-01-04 00:50:24 +0100

    First implementation of Upload and delete

---
+++ README.md
@@ -0,0 +1,33 @@
+File uploader for Deployd
+=========================
+
+Module for Deployd to upload file into a custom resource.
+
+
+Installation
+------------
+Currently, this module is not on npm yet, to install it manually, copy "index.js" in your node_modules of your deployd project and run this to install the dependencies:
+
+    npm install
+
+When this module is on npm:
+
+    npm install dpd-fileupload --save
+
+
+Configuration
+-------------
+You have to create a directory in the "public" directory of your project (default is "upload") and set its name into the dashboard settings.
+
+Notes
+-----
+Currently, you have to set the content type to "multipart/form-data".
+Only UPLOAD (POST OR PUT) and DELETE works.
+GET is planned, we could remove the upload directory from public and stream the files, that would allow to add some restrictions.
+
+Todo
+----
+- Clean returned values (in case of multiple files, headers already sent)
+- Find a better way to get the path of the upload directory
+- Implement GET
+-
---
+++ index.js
@@ -0,0 +1,155 @@
+"use strict";
+
+/**
+ * Module dependencies
+ */
+var Resource   = require('deployd/lib/resource'),
+    httpUtil   = require('deployd/lib/util/http'),
+    util       = require('util'),
+    path       = require('path'),
+    formidable = require('formidable'),
+    fs         = require('fs');
+
+/**
+ * Module setup.
+ */
+function Fileupload(options) {
+    Resource.apply(this, arguments);
+    this.config = {
+        directory: this.config.directory || 'upload',
+        fullDirectory: __dirname + "/../../public/" + (this.config.directory || '/upload') + "/"
+    };
+}
+
+util.inherits(Fileupload, Resource);
+
+Fileupload.label = "File upload";
+Fileupload.events = ["get", "upload", "delete"];
+Fileupload.prototype.clientGeneration = true;
+Fileupload.basicDashboard = {
+    settings: [
+        {
+            name: 'directory',
+            type: 'text',
+            description: 'Directory to save the uploaded files. Defaults to \'upload\'.'
+        }
+    ]
+};
+
+/**
+ * Module methods
+ */
+Fileupload.prototype.handle = function (ctx, next) {
+    var req = ctx.req,
+        self = this,
+        domain = {url: ctx.url};
+
+    if (req.method === "POST" || req.method === "PUT") {
+        var form = new formidable.IncomingForm();
+        var uploadDir = this.config.fullDirectory;
+        console.log("UploadDir: ", uploadDir);
+        form.uploadDir = uploadDir;
+        var remaining = 0;
+        var files = [];
+        var error;
+
+        var uploadedFile = function(err) {
+            if (err) {
+                error = err;
+                return ctx.done(err);
+            } else if (!err) {
+                remaining--;
+                if (remaining <= 0) {
+                    if (req.headers.referer) {
+                        httpUtil.redirect(ctx.res, req.headers.referer || '/');
+                    } else {
+                        ctx.done(null, { statusCode: 200, message: 'Files (' + files.join(", ") + ') successfully uploaded' });
+                    }
+                }
+            }
+        };
+
+        form.parse(req)
+            .on('file', function(name, file) {
+                remaining++;
+
+                if (self.events.upload) {
+                    self.events.upload.run(ctx, {url: ctx.url, fileSize: file.size, fileName: ctx.url}, function(err) {
+                        if (err) return uploadedFile(err);
+                        fs.rename(file.path, __dirname + self.config.directory + "/" + ctx.url, function(err) {
+                            if (err) uploadedFile(err);
+                            // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+                            fs.unlink(file.path, function(err) {
+                                if (err) uploadedFile(err);
+                                files.push(file);
+                                uploadedFile();
+                            });
+                        });
+                    });
+                } else {
+                    fs.rename(file.path, uploadDir + file.name, function(err) {
+                        if (err) return uploadedFile(err);
+                        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+                        fs.unlink(file.path, function(err) {
+                            if (err) uploadedFile(err);
+                            files.push(file);
+                            uploadedFile();
+                        });
+                    });
+                }
+            })
+            .on('error', function(err) {
+                ctx.done(err);
+                error = err;
+            });
+        req.resume();
+        return;
+    } else if (req.method === "GET") {
+        if (ctx.res.internal) return next(); // This definitely has to be HTTP.
+
+        if (this.events.get) {
+            this.events.get.run(ctx, domain, function(err) {
+                if (err) return ctx.done(err);
+                self.get(ctx, next);
+            });
+        } else {
+            this.get(ctx, next);
+        }
+
+    } else if (req.method === "DELETE") {
+
+        if (this.events['delete']) {
+            this.events['delete'].run(ctx, domain, function(err) {
+                if (err) return ctx.done(err);
+                self.del(ctx, next);
+            });
+        } else {
+            this.del(ctx, next);
+        }
+    } else {
+        next();
+    }
+};
+
+
+Fileupload.prototype.get = function(ctx, next) {
+    var self = this;
+    var req = ctx.req;
+    var url = req.protocol + "://" + req.get('host') + req.url + ctx.url;
+    console.log("URL: ", url);
+
+    httpUtil.redirect(ctx.res, url);
+};
+
+// Delete a file
+Fileupload.prototype.del = function(ctx, next) {
+    fs.unlink(this.config.fullDirectory + ctx.url, function(err){
+        if (err) ctx.done(err);
+        ctx.done(null, {message: 'File ' + ctx.url + ' successfully deleted'});
+    });
+};
+
+/**
+ * Module export
+ */
+module.exports = Fileupload;
